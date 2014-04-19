99b60c5efef89d95504583bf97edb8d2b2a7b133
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-04-19 18:51:48 -0500

    Initial Commit 2

---
+++ lensplease/lensplease/settings/common.py
@@ -0,0 +1,47 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+from unipath import Path
+
+PROJECT_DIR = Path(__file__).ancestor(3)
+MEDIA_ROOT = PROJECT_DIR.child("media")
+STATIC_ROOT = PROJECT_DIR.child("static")
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child("templates"),
+)
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'lensplease.urls'
+
+WSGI_APPLICATION = 'lensplease.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
---
+++ lensplease/lensplease/settings/development.py
@@ -0,0 +1,31 @@
+"""
+settngs/development.py
+Development settings for lensplease.
+
+The development environment is light and local. All data is stored underneath
+the development virtualenv.
+"""
+import os
+
+from .common import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "lensplease",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
+INSTALLED_APPS += ("debug_toolbar", )
+INTERNAL_IPS = ("127.0.0.1",)
+MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+
+if 'LENSPLEASE_CONFIG_PATH' in os.environ:
+    execfile(os.environ['LENSPLEASE_CONFIG_PATH'])
