1206ad2604c5d5072b6d78012be2516634a6d420
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-07-08 20:53:50 -0500

    Updated extras

--- lensplease/lensplease/settings/development.py
+++ lensplease/lensplease/settings/development.py
@@ -26,6 +26,7 @@ DATABASES = {
 INSTALLED_APPS += ("debug_toolbar", )
 INTERNAL_IPS = ("127.0.0.1",)
 MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+SECRET_KEY = 's'
 
 if 'LENSPLEASE_CONFIG_PATH' in os.environ:
     execfile(os.environ['LENSPLEASE_CONFIG_PATH'])
