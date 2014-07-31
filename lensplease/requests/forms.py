e6181a8714f8f7df718e72e106f4fdc235e0ac1b
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-07-30 22:42:13 -0500

    Fixed BootStrap Slider
    The Javascript isn’t pretty right now, and neither is the CSS, but it
    works and can be fixed. (Hopefully by csoltas)

--- lensplease/requests/forms.py
+++ lensplease/requests/forms.py
@@ -11,7 +11,7 @@ class PhotographerRequestForm(ModelForm):
             'last_name': forms.TextInput(attrs={'placeholder': 'Last name', 'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),
             'organization': forms.TextInput(attrs={'placeholder': 'Organization (optional)', 'class': 'form-control'}),
-            'max_price': forms.TextInput(attrs={'placeholder': ' ', 'class': 'form-control', 'type': 'range'}),
+            'max_price': forms.TextInput(attrs={'data-slider-id=': 'id_max_priceSlider', 'data-slider-min': 10, 'data-slider-max': 200, 'data-slider-step': 1, 'data-slider-value': 25, 'type': 'range'}),
             'date_needed': forms.DateInput(attrs={'placeholder': 'mm/dd/yyyy', 'class': 'form-control', 'type': 'date'}),
             'message': forms.Textarea(attrs={'placeholder': 'Howdy! I\'m looking for...', 'class': 'form-control'}),
         }
