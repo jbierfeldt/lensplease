68b89153e812486a9870ddcf8b7c011d429faade
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-08-07 12:32:37 -0500

    Improved Photographer Sign-Up Form

--- lensplease/photographer/signals.py
+++ lensplease/photographer/signals.py
@@ -1,14 +1,14 @@
 from registration.signals import user_registered
-from registration.backends.default.views import RegistrationView
-from django.core.signals import request_finished
+from photographer.views import PhotographerRegistrationView
 from photographer.models import PhotographerProfile
 from django.contrib.auth.models import User
 
 def createPhotographerProfile(sender, request, **kwargs):
+    print "working!"
     profile = PhotographerProfile()
     profile.user = kwargs["user"]
     profile.location = request.POST["location"]
     profile.affiliation = request.POST["affiliation"]
     profile.save()
 
-user_registered.connect(createPhotographerProfile, sender=RegistrationView)
+user_registered.connect(createPhotographerProfile, sender=PhotographerRegistrationView)
--- lensplease/photographer/urls.py
+++ lensplease/photographer/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, url
 from photographer.forms import LandingPhotographerRegistrationForm
-from registration.backends.default.views import RegistrationView
+from django.views.generic.base import TemplateView
+from photographer.views import PhotographerRegistrationView, PhotographerActivationView
 
 urlpatterns = patterns('',
-    url(r'^register/$', RegistrationView.as_view(form_class=LandingPhotographerRegistrationForm, template_name="landing/photographerregistration_form.html"), name='photographer_register'),
+    url(r'^registration/register/$', PhotographerRegistrationView.as_view(form_class=LandingPhotographerRegistrationForm), name='photographer_register'),
+    url(r'^registration/complete/$', TemplateView.as_view(template_name='photographer/registration_complete.html'), name='photographerregistration_complete'),
+    url(r'^registration/activate/(?P<activation_key>\w+)/$', PhotographerActivationView.as_view(), name='photographerregistration_activate'),
+    url(r'^registration/activation/complete/$', TemplateView.as_view(template_name='photographer/activation_complete.html'), name='photographer_activation_complete'),
+    url(r'^(?P<photographer_username>\w+)/$', TemplateView.as_view(template_name='photographer/test.html'), name='photographer_profile_detail'),
 )
