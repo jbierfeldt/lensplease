4f1edc1115cad74cd0b943451c4093099e06f7a0
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-08-07 12:56:56 -0500

    Moved Requests Logic to its own app

--- lensplease/landing/urls.py
+++ lensplease/landing/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
-from landing.views import PhotographerRequestCreate, LandingView
+from landing.views import LandingView
 
 urlpatterns = patterns('',
     url(r'^$', LandingView.as_view(), {}, name='landing'),
-    url(r'^add/$', PhotographerRequestCreate.as_view(), name='photographerrequest_create'),
 )
--- lensplease/requests/views.py
+++ lensplease/requests/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render_to_response
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.core.urlresolvers import reverse
 from requests.forms import LandingPhotographerRequestForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 from requests.models import PhotographerRequest
+
+class PhotographerRequestCreate(SuccessMessageMixin, CreateView):
+
+    template_name = 'requests/requests_add.html'
+    form_class = LandingPhotographerRequestForm
+    success_message = "Thanks, %(first_name)s! We'll contact you at %(email)s when we have news for you."
+        
+    def form_valid(self, form, **kwargs):
+        form.save(commit = True)
+        return super(PhotographerRequestCreate, self).form_valid(form)
+        
+    def get_success_url(self):
+        return reverse('landing')
         
 class PhotographerRequestUpdate(UpdateView):
     model = PhotographerRequest
