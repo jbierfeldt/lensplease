166cf2ad90676ae5dcaaf05259c46b6c00ab77b5
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-08-07 15:15:52 -0500

    Updated Production Server

--- lensplease/lensplease/settings/production.py
+++ lensplease/lensplease/settings/production.py
@@ -27,3 +27,10 @@ DATABASES = {
         }
         
 SECRET_KEY = os.environ["PROD_SECRET_KEY"]
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "shutterclub.io@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ["GMAIL_PASS"]
+DEFAULT_FROM_EMAIL = 'shutterclub.io@gmail.com'
