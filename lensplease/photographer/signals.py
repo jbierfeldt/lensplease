4e48f8ca28661a3889f50e1b51a9406c4803b7de
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-08-11 13:10:31 -0500

    Added Login after Activation Functionality
    Affected Signals.py

--- lensplease/photographer/signals.py
+++ lensplease/photographer/signals.py
@@ -1,14 +1,21 @@
-from registration.signals import user_registered
+from registration.signals import user_registered, user_activated
 from photographer.views import PhotographerRegistrationView
 from photographer.models import PhotographerProfile
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 def createPhotographerProfile(sender, request, **kwargs):
-    print "working!"
     profile = PhotographerProfile()
     profile.user = kwargs["user"]
     profile.location = request.POST["location"]
     profile.affiliation = request.POST["affiliation"]
     profile.save()
-
+    
 user_registered.connect(createPhotographerProfile, sender=PhotographerRegistrationView)
+
+
+def login_on_activation(sender, user, request, **kwargs):
+    user.backend='django.contrib.auth.backends.ModelBackend' 
+    login(request,user)
+    
+user_activated.connect(login_on_activation)
